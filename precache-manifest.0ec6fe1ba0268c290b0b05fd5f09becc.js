251690634612eb0d7c09398d726c8ce7c54aa7c5
Zoey Kim <zoeykim@Zoeys-MacBook-Pro.local>
2020-06-09 18:42:37 +0900

    Updates

---
+++ precache-manifest.0ec6fe1ba0268c290b0b05fd5f09becc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4b27dbf4e4bb2e17b9d3dede788de9d",
+    "url": "/movie/index.html"
+  },
+  {
+    "revision": "78a5eebff48afe5fdcea",
+    "url": "/movie/static/css/main.8776b0e8.chunk.css"
+  },
+  {
+    "revision": "78a8c6d4641937053d35",
+    "url": "/movie/static/js/2.19735fbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie/static/js/2.19735fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78a5eebff48afe5fdcea",
+    "url": "/movie/static/js/main.fb96b0d8.chunk.js"
+  },
+  {
+    "revision": "4ed6e59f9f63f770dab2",
+    "url": "/movie/static/js/runtime-main.d7b80f99.js"
+  }
+]);
